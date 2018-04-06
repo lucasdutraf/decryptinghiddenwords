a0ccbfe5426a871199f5c4e98c4bcbf3720adba6
Lucas Dutra <ldutra98@gmail.com>
2018-04-06 00:01:17 -0300

    Ideias iniciais de estruturas de métodos, notações e comandos provavelmente necessários

--- inc/processamento_de_imagem.hpp
+++ inc/processamento_de_imagem.hpp
@@ -1,10 +1,24 @@
 #ifndef PROCESSAMENTO_DE_IMAGEM_HPP
 #define PROCESSAMENTO_DE_IMAGEM_HPP
+#include <fstream>
+#include <string>
+using namespace std;
 
 class ProcessamentoDeImagem{
+    private:
+        int largura, altura;
+        char id;
+        int ValorMaximo;
+        string caminho;
+    
+    public:
+        void setPath(string caminho);
+        string getPath();
+
+        void leArquivo();
+
+        
 
 
-}
-
-
+};
 #endif
--- src/processamento_de_imagem.cpp
+++ src/processamento_de_imagem.cpp
@@ -1,4 +1,29 @@
 #include "processamento_de_imagem.hpp"
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+
+//ofstream -> saida sistema e entrada no arquivo
+/*  ofstream arquivo;
+    arquivo.open("caminho_do_arquivo.pgm");
+    arquivo.open("conteudo", ios::app); -> não sobrescreve conteúdo
+    arquivo << "O que se deseja armazenar no arquivo"; -> sobrescrevendo
+    arquivo.close(); ->limpa o espaço de memoria*/
+
+
+
+//ifstream -> saindo do arquivo e entrando no programa
+/*  ifstream arquivo;
+    string linha; -> mostar linha na tela
+    arquivo.open("caminho_do_arquivo.*");
+    if (arquivo.is_open()){
+        while(getline(arquivo, string)){
+            cout << linha << endl;
+        }
+        arquivo.close();
+    } -> verifica se o arquivo está aberto, retorna true se sim
+
+*/
+//fstream -> entrada ou saída
+//sempre fechar o arquivo
