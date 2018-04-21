19fc1276aad5e071dbfb83052fa9a64526a94b7b
Lucas Dutra <ldutra98@gmail.com>
2018-04-21 11:50:15 -0300

    tentativa de seletor implantado na main

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,26 @@ int main (int argc, char ** argv){
     ProcessamentoDeImagemPGM process1;
     UI inter;
     string Path;
-    int seletor;
+    int seletor_main;
+    inter.InterfaceInicial();
+    inter.PedeTipoDeImagem();
+    cin >> seletor_main;
+    if (seletor_main == 1){
+        inter.RecebeImagem();
+        cin >> Path;
+        process1.setPathIn(Path);
+        process1.leitorImg();
+        process1.UncripMsg();
+
+    }
+    else if (seletor_main == 2){
+
+    }
+    else {
+        //break;
+    }
     
-    inter.RecebeImagem();
-    cin >> Path;
-    process1.setPathIn(Path);
-    process1.leitorImg();
-    process1.UncripMsg();
+
 
     return 0;
 }
